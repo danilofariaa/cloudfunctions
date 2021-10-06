4a34829b1c1e6983d15fd99713d2eb0ab529f137
Danilo faria <danilo.faria@dp6.com.br>
2021-10-06 14:10:18 -0300

    cloudfunction SFMC v1.3.1 - Asyncio

--- st_to_bq.py
+++ st_to_bq.py
@@ -2,23 +2,20 @@ import json
 import requests
 import time
 import asyncio
-from asyncio import gather, run
-from httpx import AsyncClient
 import pandas as pd
+import google
+import aiohttp
+from asyncio import gather, run
+
+# from httpx import AsyncClient
 from google.cloud import storage, bigquery
 from datetime import datetime
 
+from requests.exceptions import Timeout
+
 
 def get_data_secret(version):
-    if version == "v1":
-        credentals = {
-            "AUTH_URI_V1_RAIA": "https://mcjss9736km3nd134n6cv8-hcfy0.auth.marketingcloudapis.com/v1/requestToken",
-            "REST_URI_V1_RAIA": "https://mcjss9736km3nd134n6cv8-hcfy0.rest.marketingcloudapis.com/",
-            "CLIENT_ID_V1_RAIA": "o09bav0efhzkub1fjtngg2kf",
-            "CLIENT_SECRET_V1_RAIA": "QiWxOZR0fPB3yeJUbBQMxE6G",
-        }
-        return credentals
-    elif version == "v2":
+    if version == "v2":
         credentals = {
             "AUTH_URI_V2_RAIA": "https://mcjss9736km3nd134n6cv8-hcfy0.auth.marketingcloudapis.com/v2/token",
             "REST_URI_V2_RAIA": "https://mcjss9736km3nd134n6cv8-hcfy0.rest.marketingcloudapis.com/",
@@ -32,16 +29,9 @@ def get_data_secret(version):
 
 def get_data_credentials(version):
     # Args: Version(str) : Api version
-    # 2nd step get credentials from .env file
+    # 2 Passo : Coletar as informacoes de data secret, como endpoint para autenticacao,consumo bem como client e client secret
     c = get_data_secret(version)
-
-    if version == "v1":
-        credendials = {
-            "clientId": c["CLIENT_ID_V1_RAIA"],
-            "clientSecret": c["CLIENT_SECRET_V1_RAIA"],
-        }
-        return credendials
-    elif version == "v2":
+    if version == "v2":
         credendials = {
             "client_id": c["CLIENT_ID_V2_RAIA"],
             "client_secret": c["CLIENT_SECRET_V2_RAIA"],
@@ -54,7 +44,7 @@ def get_data_credentials(version):
 
 def get_credendials(version):
     # Args: Version(str) : Api version
-    # 1st step get credentials from dotenv and return credentials
+    # 1 Passo : Pegar as credenciais do usuario para logar na API
     credentials = get_data_credentials(version)
     return credentials
 
@@ -85,6 +75,7 @@ def response_data(url, payload):
     check_status, status = check_status_code(response.status_code)
     body = json.loads(response.content)
     if check_status == "success":
+        print("Autenticado")
         auth_response = {
             "token": body["access_token"],
             "token_type": body["token_type"],
@@ -119,22 +110,54 @@ def get_auth(payload, version):
     return str(authentication)
 
 
-async def download(number, token):
-    url = "https://mcjss9736km3nd134n6cv8-hcfy0.rest.marketingcloudapis.com/data/v1/customobjectdata/key/4B16816C-B017-418C-9378-C4B0A28B0ED3/rowset?$pageSize=2500&$page={number}"
+async def download(session, url, token):
+    """
+    Task para realizar requisicao assincrona
+    Args =  number : numero da iteracao
+            token: token para autenticacao com api
+            externalkey: chave para acesso a dataextension
+    """
     headers = {"Authorization": token}
-    async with AsyncClient() as client:
-        response = await client.get(
-            url.format(number=number), headers=headers, timeout=None
-        )
-        print(number)
-        return response
+    async with session.get(url=url, headers=headers, timeout=None) as resp:
+        data = await resp.json()
+        return data
 
 
-async def consume_sfmc(start, stop, token):
-    return await gather(*[download(number, token) for number in range(start, stop)])
+async def consume_sfmc(start, stop, token, externalkey):
+    """
+    A API possui uma restrição de 2500 registros por requisicao, dessa forma implementamos um loop para iterar sob o total de registros solicitado,
+    dado pelo total de registros / 2500
+    Args = start: range inicial para o consumo da api
+           stop : range final para o consumo da api
+           token : token de autenticacao sfmc
+           externalkey : external key vinda da data extensions SFMC
+    """
+    print("Consumindo dados")
+    connector = aiohttp.TCPConnector(limit=20)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        tasks = []
+        for number in range(start, stop):
+            url = "https://mcjss9736km3nd134n6cv8-hcfy0.rest.marketingcloudapis.com/data/v1/customobjectdata/key/{externalkey}/rowset?$pageSize=2500&$page={number}"
+            tasks.append(
+                asyncio.ensure_future(
+                    download(
+                        session,
+                        url.format(externalkey=externalkey, number=number),
+                        token,
+                    )
+                )
+            )
+            await asyncio.sleep(0.001)
+
+        pii_data = await asyncio.gather(*tasks)
+        return pii_data
 
 
 def unpack(list):
+    """
+    Desempacota os dados piis vindos da requisicao
+    Args = lista de dados da requisicao  (vindas do request.content)
+    """
     lst = []
     for item in list:
         for key, value in item.items():
@@ -143,51 +166,29 @@ def unpack(list):
     return lst
 
 
-def create_dataframe(data):
+def create_dataframe(data, de_key, de_fields):
     # Args
-    # data : dict with SFMC
-    # Create pandas dataframe and fill NaN values
-    df = pd.DataFrame(
-        data,
-        columns=[
-            "golden_id",
-            "nome",
-            "sobrenome",
-            "email",
-            "tel_celular",
-            "tel_fixo_res",
-            "cliente_raia",
-            "cliente_drogasil",
-            "cliente_univers",
-            "possui_app",
-            "optout_email",
-            "optout_sms",
-            "optout_midias",
-            "possui_app_raia",
-            "possui_app_drogasil",
-            "bandeira_pref",
-        ],
-    )
+    # data : dict com os dados piis vindos da SFMC
+    # Cria um dataframe e preenche dados NaN como NULL
+    df = pd.DataFrame(data, columns=de_fields[de_key])
     df.fillna("NULL", inplace=True)
 
     return df
 
 
-def get_data_list(em):
-    dl = [json.loads(x.decode("utf-8")) for x in em]
-    return dl
-
-
-def csv_to_gcs(dataframe, brand: str, config):
+def csv_to_gcs(dataframe, data_extension: str, config):
     # Args
     # dataframe : dict with SFMC
     # brand : Raia or Drogasil
     # config : Project initial configs such as (project, bucketname)
     # Save file on GCS bucket (adsync)
     date = datetime.today().strftime("%Y-%m-%d")
-    filepath = f"{brand}_pii_{date}.csv"
+    filepath = f"{data_extension}_pii_{date}.csv"
     uri = save_file_on_bucket(
-        dataframe.to_csv(index=False), config["project"], config["bucketname"], filepath
+        dataframe.to_csv(index=False),
+        config["project"],
+        config["bucketname"][0],
+        filepath,
     )
     return uri
 
@@ -195,13 +196,18 @@ def csv_to_gcs(dataframe, brand: str, config):
 def save_file_on_bucket(
     content: bytes,
     project: str,
-    bucketname: str,
+    bucketname: list,
     filepath: str,
     content_type="text/plain",
 ):
-    # Args
-    # data : dict with SFMC
-    # Create pandas dataframe and fill NaN values
+    """
+    Salva o csv com piis no bucket do GCS
+    Args = content
+           project : arquivo csv
+           bucketname : nome do bucket GCS
+           filepath : caminho salvo no GCS
+           content-type : tipo de conteudo
+    """
 
     storage_client = storage.Client(project)
     uri = f"gs://{bucketname}/{filepath}"
@@ -221,7 +227,8 @@ def save_file_on_bucket(
 
 
 def create_schema(df):
-    # Create schema for BQ Table
+    # Cria schema para uma tabela do BQ
+    # Args = df: Dataframe pandas criado para os dados piis
     lst = df.columns.tolist()
     schema = []
     types = {"int": "INTEGER", "float": "FLOAT", "bool": "BOOLEAN", "str": "STRING"}
@@ -234,12 +241,19 @@ def create_schema(df):
     return schema
 
 
-def gcs_to_bq(project, dataset_name, brand, schema, gcs_uri):
-
+def gcs_to_bq(project, dataset_name, data_extension, schema, gcs_uri):
+    """
+    Salva o arquivo csv com dados piis no Big Query
+    Args: project
+          dataset_name = nome do dataset
+          brand = bandeira (drogasil ou raia)
+          schema = schema da tabela
+          gcs_uri = uri do arquivo csv no gcs
+    """
     print("Inicializando Client .. ")
     bq_client = bigquery.Client(project)
     dataset_ref = bq_client.get_dataset(project + "." + dataset_name)
-    table_name = f"{brand}_{datetime.today().strftime('%Y-%m-%d')}"
+    table_name = f"{data_extension}_{datetime.today().strftime('%Y-%m-%d')}"
     print(f"Reporting  \n {dataset_name} => {table_name}  \n  {schema} \n {gcs_uri}")
     print("configurando job ... ")
     job_config = bigquery.LoadJobConfig(
@@ -261,47 +275,152 @@ def gcs_to_bq(project, dataset_name, brand, schema, gcs_uri):
 
 
 def clean_pii(dados_pii):
+    """
+    Para cada requisicao, pega apenas os dados da chave items vindas da request
+    Args =  dados_pii: lista de requests
+    """
     lst = []
+    count = 0
     for d in dados_pii:
-        body = json.loads(d.content)
-        for key, value in body.items():
+        # body = json.loads(d.content)
+        for key, value in d.items():
             if key == "items":
+                count += 1
                 lst.append(value)
+    print(f"total de items {count}")
+    return lst
+
+
+def merge_pii(dp_clean):
+    """
+    Lista com dicionarios com todas as informacoes completas juntas
+    Args : Dicionario com dados piis tratados
+
+    """
+
+    lst = []
+    for i in dp_clean:
+        for j in i:
+            a = {**j["keys"], **j["values"]}
+            lst.append(a)
+    return lst
+
+
+def extract_keys(data_extension):
+    """
+    Carrega as chaves da data extensions para formacao de nome de tabela
+    Args = data_extension nome das chaves do dicionario de data extensions
+    """
+    lst = []
+    de = data_extension.keys()
+    for i in de:
+        lst.append(i)
     return lst
 
 
 if __name__ == "__main__":
     # API Version V2 uses OAuth2.0
-    v = "v2"
-    brand = "raia"
-    # Get environments credencials for consume API
-    p = get_credendials(v)
-    # Authenticate with SFMC API
-    t = get_auth(p, v)
-    # Consume the API using the endpoints of SFMC docs
+    # Dicionario de dados com configurações iniciais : Versão API, Bandeira
+    cfg_initial = {"version": "v2", "brand": ["raia", "drogasil"]}
+
+    # Dicionario com as data extensions e external keys
+    data_extension = {
+        "MCV_LGPD_RAIA": "50594300-03D9-485D-9E4D-39CB331023B6",  # ok
+        "MCV_LGPD_DROGASIL": "C1E4F6ED-DFDC-4E8A-AB57-A94FE2F2878C",  # ok
+        "MCV_CAD_CLIENTE": "4B16816C-B017-418C-9378-C4B0A28B0ED3",
+        "MCV_DNA": "C9DE68B4-E324-496C-B8A1-A7BC384720B3",
+    }
+
+    data_extension_fields = {
+        "MCV_LGPD_RAIA": [
+            "golden_id",
+            "bkl_lgpd_telefone",
+            "bkl_lgpd_celular",
+            "bkl_lgpd_push",
+            "bkl_lgpd_email",
+        ],
+        "MCV_LGPD_DROGASIL": [
+            "golden_id",
+            "bkl_lgpd_telefone",
+            "bkl_lgpd_celular",
+            "bkl_lgpd_push",
+            "bkl_lgpd_email",
+        ],
+        "MCV_CAD_CLIENTE": [
+            "golden_id",
+            "nome",
+            "sobrenome",
+            "email",
+            "tel_celular",
+            "tel_fixo_res",
+            "cliente_raia",
+            "cliente_drogasil",
+            "optout_email",
+            "bandeira_pref",
+        ],
+        "MCV_DNA": [
+            "golden_id",
+            "alergia_e_renite",
+            "asma",
+            "barba",
+            "bebe",
+            "cuidado_senior",
+            "curativos",
+            "diabetes_oral",
+            "gestante",
+            "higiene_basica",
+            "higiene_feminina",
+            "higiene_oral",
+            "hipertensao",
+            "ex_fiel_raia",
+            "ex_fiel_drog",
+            "churn_fiel_raia",
+            "churn_fiel_drog",
+            "multicanal",
+            "digital",
+            "dna_cronico",
+            "perfil_beleza",
+        ],
+    }
+
+    data_extension_keys = extract_keys(data_extension)
+
+    # Busca as credenciais para autenticar na API
+    credentials = get_credendials(cfg_initial["version"])
+    # Autenticação na SFMC API
+    auth = get_auth(credentials, cfg_initial["version"])
+
+    # Consome a SFMC API atraves de seus endpoints de forma assincrona para buscar dados PII's
     start_consume_sfmc = time.time()
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    loop = asyncio.ProactorEventLoop()
-    asyncio.set_event_loop(loop)
-    dados_piis = run(consume_sfmc(1, 200, t))
+    asyncio.set_event_loop_policy(
+        asyncio.WindowsSelectorEventLoopPolicy()
+    )  # Police para Windows
+    dados_piis = run(consume_sfmc(1, 401, auth, data_extension["MCV_DNA"]))
+
     end_consume_sfmc = time.time() - start_consume_sfmc
     print(f" SFMC Excution Time: {end_consume_sfmc}")
 
-    d = clean_pii(dados_piis)
-    lst = []
-    for i in d:
-        for j in i:
-            a = {**j["keys"], **j["values"]}
-            lst.append(a)
+    # Desempacotamento/Tratamento dos dados vindos do consumo da SFMC API
+    dp_clean = clean_pii(dados_piis)
 
-    # Create dataframe
-    df = create_dataframe(lst)
+    # Merging de dados vindos do processo de desempacotamento para transformar multiplos dicionarios com dados piis em um
+    merged_data = merge_pii(dp_clean)
+
+    # Transformação de dicionario de dados para dataframe
+    df = create_dataframe(merged_data, data_extension_keys[3], data_extension_fields)
     print(df)
-    # Set config for project
-    config = {"project": "raiadrogasil-280519", "bucketname": "drogaraia_adsync"}
-    # Uri for save csv file into BQ
-    uri = csv_to_gcs(df, brand, config)
-    # Collect Schema from packed data
+
+    # Dicionario de configuração para inserir arquivos CSV no GCS
+    config = {
+        "project": "raiadrogasil-280519",
+        "bucketname": ["drogaraia_adsync", "drogasil_adsync"],
+    }
+
+    # Inserção do arquivo CSV (com dados piis) no GCS
+    uri = csv_to_gcs(df, data_extension_keys[3], config)
+
+    # Cria o schema das colunas do dataframe. Será passado como parametro na construção da tabela no BQ
     schema = create_schema(df)
-    # Save GCS csv file into BQ
-    gcs_to_bq(config["project"], "data_adsync", brand, schema, uri)
+
+    # Salva os dados Piis no BQ
+    gcs_to_bq(config["project"], "data_adsync", data_extension_keys[3], schema, uri)
